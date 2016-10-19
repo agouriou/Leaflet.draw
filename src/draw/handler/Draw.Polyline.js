6f5f3a43ed13dbc974dc7a4ee00df766bbe63174
Armel Gouriou <armel.gouriou@adventiel.fr>
2016-10-19 16:48:39 +0200

    fix: touch event on IE11

--- src/draw/handler/Draw.Polyline.js
+++ src/draw/handler/Draw.Polyline.js
@@ -94,15 +94,16 @@ L.Draw.Polyline = L.Draw.Feature.extend({
 				.addTo(this._map);
 
                         //FIX for touch on IE11 and some browsers on windows
-			if (L.Browser.touch) { // #TODO: get rid of this once leaflet fixes their click/touch.
+			/*if (L.Browser.touch) { // #TODO: get rid of this once leaflet fixes their click/touch.
 				this._map
 				  .on('click', this._onTouch, this);
 		        } else {
 				this._map
 				  .on('mouseup', this._onMouseUp, this); // Necessary for 0.7 compatibility
-			}
+			}*/
 
 			this._map
+				.on('mouseup', this._onMouseUp, this)
 				.on('mousemove', this._onMouseMove, this)
 				.on('zoomlevelschange', this._onZoomEnd, this)
 				.on('zoomend', this._onZoomEnd, this);
